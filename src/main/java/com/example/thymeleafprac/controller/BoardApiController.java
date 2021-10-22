6d3871718e8e3fa04e5f713fa2fee8807dfa9341
dma963 <dma963@naver.com>
2021-10-22 18:06:31 +0900

    cheapter 7. Restful API 사용

---
+++ src/main/java/com/example/thymeleafprac/controller/BoardApiController.java
@@ -0,0 +1,58 @@
+package com.example.thymeleafprac.controller;
+
+import com.example.thymeleafprac.model.Board;
+import com.example.thymeleafprac.repository.BoardRepository;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BoardApiController {
+
+    private final BoardRepository boardRepository;
+
+    public BoardApiController(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
+                    @RequestParam(required = false, defaultValue = "") String content) {
+        if (StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+            return boardRepository.findAll();
+        } else {
+            return boardRepository.findByTitleOrContent(title, content);
+        }
+    }
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return boardRepository.save(newBoard);
+    }
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return boardRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/boards/{id")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+        return boardRepository.findById(id)
+                .map(board -> {
+                    board.setId(newBoard.getId());
+                    board.setContent(newBoard.getContent());
+                    return boardRepository.save(board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return boardRepository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        boardRepository.deleteById(id);
+    }
+}
