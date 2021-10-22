74b57f801bce96396d7ff1783bc0af579234f51f
dma963 <dma963@naver.com>
2021-10-22 17:18:54 +0900

    cheapter 6. Form Data 설정

--- src/main/java/com/example/thymeleafprac/controller/BoardController.java
+++ src/main/java/com/example/thymeleafprac/controller/BoardController.java
@@ -2,25 +2,51 @@ package com.example.thymeleafprac.controller;
 
 import com.example.thymeleafprac.model.Board;
 import com.example.thymeleafprac.repository.BoardRepository;
+import com.example.thymeleafprac.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
 
     @GetMapping("/list")
     public String list(Model model) {
         List<Board> boardList = boardRepository.findAll();
         model.addAttribute("boards", boardList);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String boardSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
