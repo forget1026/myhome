7f92df4c7beacd0b140a3eaa60b2e538b59207c7
dma963 <dma963@naver.com>
2021-10-22 14:41:56 +0900

    cheapter 5. 게시판 조회

--- src/main/java/com/example/thymeleafprac/controller/BoardController.java
+++ src/main/java/com/example/thymeleafprac/controller/BoardController.java
@@ -1,14 +1,26 @@
 package com.example.thymeleafprac.controller;
 
+import com.example.thymeleafprac.model.Board;
+import com.example.thymeleafprac.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boardList = boardRepository.findAll();
+        model.addAttribute("boards", boardList);
         return "board/list";
     }
 }
