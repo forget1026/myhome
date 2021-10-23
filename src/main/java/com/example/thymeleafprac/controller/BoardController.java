9fc9e1600ed334e21ebe4004ef6ee664c771f206
dma963 <dma963@naver.com>
2021-10-23 20:27:52 +0900

    cheapter 7. JPA를 이용한 페이지 처리 및 검색

--- src/main/java/com/example/thymeleafprac/controller/BoardController.java
+++ src/main/java/com/example/thymeleafprac/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.example.thymeleafprac.model.Board;
 import com.example.thymeleafprac.repository.BoardRepository;
 import com.example.thymeleafprac.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,8 +26,15 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boardList = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Board> boardList = boardRepository.findAll(pageable);
+        Page<Board> boardList = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(boardList.getPageable().getPageNumber() - 4, 1);
+        int endPage = Math.min(boardList.getTotalPages(), boardList.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boardList);
         return "board/list";
     }
