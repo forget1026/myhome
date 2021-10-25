67f164b29aeced4f5d06c498cb247d5324785a42
dma963 <dma963@naver.com>
2021-10-25 15:52:11 +0900

    cheapter 9. Spring Security를 이용한 로그인 처리

---
+++ src/main/java/com/example/thymeleafprac/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.thymeleafprac.repository;
+
+import com.example.thymeleafprac.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
